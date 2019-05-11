3a2a61b530b7a86e0e5a22f2b897a9463e38f552
lolimay <lolimay@lolimay.cn>
2019-05-12 01:53:56 +0800

    Init the repo

---
+++ Counter.js
@@ -0,0 +1,30 @@
+/*eslint-disable no-unused-vars */
+import React, { Component, PropTypes } from 'react'
+
+const Counter = ({ value, onIncrement, onDecrement, onIncrementIfOdd }) =>
+      <div>
+        <button onClick={onIncrement}>
+          Increment
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          Decrement
+        </button>
+        {' '}
+        <button onClick={onIncrementIfOdd}>
+          IncrementIfOdd
+        </button>
+        <hr />
+        <div>
+          Clicked: {value} times
+        </div>
+      </div>
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+  onIncrementIfOdd: PropTypes.func.isRequired
+}
+
+export default Counter
---
+++ main.js
@@ -0,0 +1,44 @@
+import "babel-polyfill"
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import Counter from './Counter'
+import reducer from './reducers'
+import helloSaga from './sagas'
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+const store = createStore(
+  reducer,
+  applyMiddleware(createSagaMiddleware(helloSaga))
+)
+
+// You can use subscribe() to update the UI in response to state changes.
+// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+// However it can also be handy to persist the current state in the localStorage.
+store.subscribe( () => {
+  console.log(store.getState())
+})
+
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed.
+const action = type => store.dispatch({type})
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')}
+      onIncrementIfOdd={() => action('INCREMENT_IF_ODD')} />,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
+saga
