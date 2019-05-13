ee18481c878bde481de223c5e619c64e079154ca
lolimay <lolimay@lolimay.cn>
2019-05-13 19:05:34 +0800

    Add some notes for reducer's usage

--- reducers/random.js
+++ reducers/random.js
@@ -6,6 +6,17 @@ const initialState = {
 	hasMessage: false
 };
 
+//! Things you should never do inside a reducer:
+// Mutate its arguments;
+// Perform side effects like API calls and routing transitions;
+// Call non-pure functions, e.g. Date.now() or Math.random().
+//! The reducer must be pure:
+// TODO Given the same arguments, it should calculate the next state and return it.
+// TODO - No surprises.
+// TODO - No side effects.
+// TODO - No API calls.
+// TODO - No mutations.
+// TODO  Just a **calculation**.
 export default function random(state = initialState, action) {
 	switch (action.type) {
 		case types.RANDOM.TOGGLE_LOGGING:
